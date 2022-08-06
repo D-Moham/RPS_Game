c4f7a447e21e842294b58c8dc31b2528d46f8236
D-Moham <86933814+D-Moham@users.noreply.github.com>
2022-08-06 03:44:20 -0400

    Minor fixes and tried to implement recursion
    Recursion implementation failed, will need to find a way to implement recursion with the method

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,25 +1,33 @@
 import random
 
 def play():
-    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors \n")
-    computer =random.choice(['r', 'p', 's'])
+    while True:
+        user = input("What's your choice? 'rock' for rock, 'paper' for paper, 'scissors' for scissors \n")
+        computer =random.choice(['rock', 'paper', 'scissors'])
 
-    print(f'Computer chose: {computer}')
+        print(f'You chose: {user}')
+        print(f'Computer chose: {computer}')
 
-    if user == computer:
-        return 'It\'s a tie'
+        if user == computer:
+            return 'It\'s a tie'
 
-    # r>s, s>p, p>r
-    if is_win(user, computer):
-        return 'You won!'
+        # r>s, s>p, p>r
+        if is_win(user, computer):
+            return 'You won!'
 
-    return 'You lost!'
+        else: print('You lost!')
+
+        play_again = input("Play again? (y/n): ")
+        if play_again.lower() != "y":
+            break
+
+
 
 def is_win(player, opponent):
     #return true if player wins
     #r > s, s > p, p > r
-    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
-        or (player == 'p' and opponent == 'r'):
+    if (player == 'rock' and opponent == 'scissors') or (player == 'scissors' and opponent == 'paper') \
+        or (player == 'paper' and opponent == 'rock'):
         return True
 
 print(play())
